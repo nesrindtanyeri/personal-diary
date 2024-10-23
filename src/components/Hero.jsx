9cf728ea8e9fd14afe14adbbe74996ab700f6ba6
Gerd Braun <webmaster@your-d-sign.de>
2024-10-23 10:56:13 +0200

    added daisy ui

---
+++ src/components/Hero.jsx
@@ -0,0 +1,30 @@
+const imgUrl = new URL('../assets/bg.jpg', import.meta.url).href;
+
+const Hero = ({toggleModal}) => {
+    console.log(imgUrl);
+  return (
+    <div
+    className="hero min-h-screen"
+    style={{
+      backgroundImage:`url(${imgUrl})`,
+    }}
+  >
+    <div className="hero-overlay bg-opacity-60"></div>
+    <div className="hero-content text-neutral-content text-center">
+      <div className="max-w-md">
+        <h1 className="mb-5 text-5xl font-bold">Personal Diary</h1>
+        <p className="mb-5">
+          Provident cupiditate voluptatem et in. Quaerat fugiat ut
+          assumenda excepturi exercitationem quasi. In deleniti eaque aut
+          repudiandae et a id nisi.
+        </p>
+        <button className="btn btn-primary" onClick={toggleModal}>
+          Get Started
+        </button>
+      </div>
+    </div>
+  </div>
+)
+}
+
+export default Hero
