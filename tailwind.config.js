0666464d288bc3de7aff055ec6676c2064ee482d
Gerd Braun <webmaster@your-d-sign.de>
2024-10-25 11:50:06 +0200

    different alert colors shoud work now

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 
-import daisyui from 'daisyui';
+import daisyui from "daisyui";
 
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  safelist: ["alert-success", "alert-error"],
   theme: {
-    extend: {}
+    extend: {},
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes:["light", "dark", "dracula","aqua"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-  }
+    themes: ["light", "dark", "dracula", "aqua"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+  },
 };
